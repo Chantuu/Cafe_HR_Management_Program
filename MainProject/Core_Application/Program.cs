6bf65bcc942496442dad6e42f8b09f161663eda9
Chantuu <Chantuu@CHANTUU_>
2023-08-21 17:34:38 +0400

    Create Main Menu Display

--- MainProject/Core_Application/Program.cs
+++ MainProject/Core_Application/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("***********************************");
+Console.WriteLine("**         HR Management         **");
+Console.WriteLine("***********************************");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine("1. Add New Employee");
+Console.WriteLine("2. Employee Operations");
+Console.WriteLine("3. Financial Data");
+Console.WriteLine("4. View Current Employees");
+Console.WriteLine("5. Remove Employee");
+Console.WriteLine("6. Save To File");
+Console.WriteLine("7. About Application");
+Console.WriteLine("8. Exit Application");
+
