9ded95e4286adb37c7299a6fd7bf543777504de9
Chantuu <Chantuu@CHANTUU_>
2023-08-24 17:00:46 +0400

    Implement new employee addition functionality

---
+++ MainProject/Core_Application/Misc/Utilites.cs
@@ -0,0 +1,21 @@
+﻿using Cafe_Program.HR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe_Program.Misc
+{
+    internal class Utilites
+    {
+        public static string mainMenuTitle = "***********************************\n**         HR Management         **\n***********************************";
+        public static string newEmployeeMenuTitle = "***********************************\n**       Add New Employee        **\n***********************************";
+
+        public static List<Manager> managers = new List<Manager>();
+        public static List<Accountant> accoutants = new List<Accountant>();
+        public static List<Chef> chefs = new List<Chef>();
+        public static List<Bartender> bartenders = new List<Bartender>();
+        public static List<Waitress> waitresses = new List<Waitress>();
+    }
+}
--- MainProject/Core_Application/Program.cs
+++ MainProject/Core_Application/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using Cafe_Program.HR;
+using Cafe_Program.Misc;
+using System.Runtime.InteropServices;
+
 string userInput;
 
 do
@@ -50,13 +53,7 @@ while (userInput != "8");
 
 static string MainMenu()
 {
-    Console.ForegroundColor = ConsoleColor.Cyan;
-    Console.WriteLine("***********************************");
-    Console.WriteLine("**         HR Management         **");
-    Console.WriteLine("***********************************");
-    Console.ResetColor();
-
-    Console.WriteLine();
+    DisplayTitle(Utilites.mainMenuTitle);
 
     Console.WriteLine("1. Add New Employee");
     Console.WriteLine("2. Employee Operations");
@@ -66,6 +63,7 @@ static string MainMenu()
     Console.WriteLine("6. Save To File");
     Console.WriteLine("7. About Application");
     Console.WriteLine("8. Exit Application");
+    Console.WriteLine();
     Console.Write("Your input: ");
 
     string userInput = Console.ReadLine();
@@ -74,25 +72,18 @@ static string MainMenu()
 
 static void NewEmployeeMenu()
 {
-    string title = "***********************************\n**       Add New Employee        **\n***********************************";
     string userInput;
 
     do
     {
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine(title);
-        Console.ResetColor();
-        Console.WriteLine();
-
+        DisplayTitle(Utilites.newEmployeeMenuTitle);
         Console.WriteLine("Please select role for new Employee:");
         Console.WriteLine("1. Manager");
         Console.WriteLine("2. Accountant");
         Console.WriteLine("3. Chef");
         Console.WriteLine("4. Bartender");
         Console.WriteLine("5. Waitress");
-
         DisplayBackMessage();
-
         Console.Write("Your input: ");
 
         userInput = Console.ReadLine();
@@ -101,23 +92,27 @@ static void NewEmployeeMenu()
         {
             case "1":
                 Console.Clear();
-                NewEmployeeMenu();
+                AddNewEmployeeProcess(EmployeeSelection.manager);
                 break;
 
             case "2":
                 Console.Clear();
+                AddNewEmployeeProcess(EmployeeSelection.accountant);
                 break;
 
             case "3":
                 Console.Clear();
+                AddNewEmployeeProcess(EmployeeSelection.chef);
                 break;
 
             case "4":
                 Console.Clear();
+                AddNewEmployeeProcess(EmployeeSelection.bartender);
                 break;
 
             case "5":
                 Console.Clear();
+                AddNewEmployeeProcess(EmployeeSelection.waitress);
                 break;
 
             case "menu":
@@ -128,15 +123,105 @@ static void NewEmployeeMenu()
                 break;
         }
     }
-    while(userInput != "menu");
+    while (userInput != "menu");
     
+}
+
+static void AddNewEmployeeProcess(EmployeeSelection selection)
+{
+    DisplayTitle(Utilites.newEmployeeMenuTitle);
+    Console.Write("Please input new employee name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Please input new employee surname: ");
+    string surname = Console.ReadLine();
+
+    Console.Write("Please input new employee email: ");
+    string email = Console.ReadLine();
+
+    Console.Write("Please input new employee rate: ");
+    try
+    {
+        double rate = Double.Parse(Console.ReadLine());
+
+        Console.Write("Please input employee birth date in 'MM/DD/YYYY' format: ");
+        try
+        {
+            DateOnly birthDate = DateOnly.Parse(Console.ReadLine());
+            DateOnly hireDate = DateOnly.FromDateTime(DateTime.Now);
+
+            Console.Write("Please input 1st street address of new employee: ");
+            string address1 = Console.ReadLine();
+
+            Console.Write("Please input 2nd street address of new employee (Press enter, if it is not available): ");
+            string address2 = Console.ReadLine();
+
+            Console.Write("Please input zipcode of new employee: ");
+            string zipcode = Console.ReadLine();
+
+            Console.Write("Please input city of new employee: ");
+            string city = Console.ReadLine();
+
+            switch (selection)
+            {
+                case EmployeeSelection.manager:
+                    Utilites.managers.Add(new Manager(name, surname, email, rate, birthDate, hireDate, address1, address2, zipcode, city));
+                    break;
+
+                case EmployeeSelection.accountant:
+                    Utilites.accoutants.Add(new Accountant(name, surname, email, rate, birthDate, hireDate, address1, address2, zipcode, city));
+                    break;
+
+                case EmployeeSelection.chef:
+                    Utilites.chefs.Add(new Chef(name, surname, email, rate, birthDate, hireDate, address1, address2, zipcode, city));
+                    break;
+
+                case EmployeeSelection.bartender:
+                    Utilites.bartenders.Add(new Bartender(name, surname, email, rate, birthDate, hireDate, address1, address2, zipcode, city));
+                    break;
+
+                case EmployeeSelection.waitress:
+                    Utilites.waitresses.Add(new Waitress(name, surname, email, rate, birthDate, hireDate, address1, address2, zipcode, city));
+                    break;
+
+            }
+
+            DisplaySuccessMessage("New employee has been successfully added to application!");
+
+        }
+        catch (FormatException ex)
+        {
+            DisplayErrorMessage(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            DisplayErrorMessage(ex.Message);
+        }
+
+    }
+    catch (FormatException ex)
+    {
+        DisplayErrorMessage(ex.Message);
+    }
+    catch (Exception ex)
+    {
+        DisplayErrorMessage(ex.Message);
+    }
 }
 
 static void DisplayErrorMessage(string message)
 {
     Console.Clear();
-
     Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.WriteLine();
+    Console.ResetColor();
+}
+
+static void DisplaySuccessMessage(string message)
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
     Console.WriteLine(message);
     Console.WriteLine();
     Console.ResetColor();
@@ -148,4 +233,12 @@ static void DisplayBackMessage()
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Type menu to go back to Main Menu");
     Console.ResetColor();
+}
+
+static void DisplayTitle(string title)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine(title);
+    Console.ResetColor();
+    Console.WriteLine();
 }
